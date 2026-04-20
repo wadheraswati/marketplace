df91ffb407fa50aa6ce8a8f4e8b351e76bfebc4c
wadheraswati <wadheraswati@gmail.com>
2026-04-20 01:21:40 -0400

    Initial Commit

---
+++ MarketplaceApp/MarketplaceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarketplaceAppApp.swift
+//  MarketplaceApp
+//
+//  Created by Swati Seera on 2026-04-20.
+//
+
+import SwiftUI
+
+@main
+struct MarketplaceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
